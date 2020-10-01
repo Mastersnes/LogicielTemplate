5af0c6252325b98411605b58ba960e9d622208c1
Mastersnes <steven.nesztler@gmail.com>
2020-10-01 18:16:13 +0200

    Mise en place du lancement JAVA

--- src/main/java/com/ititi/App.java
+++ src/main/java/com/ititi/App.java
@@ -1,25 +1,42 @@
 package com.ititi;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    protected Logger logger = Logger.getLogger(getClass().getName());
 
     @Override
-    public void start(final Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    public void start(final Stage primaryStage) {
+        primaryStage.setTitle("First Stage");
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(500);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        final Stage secondStage = new Stage();
+        secondStage.setTitle("Second Stage");
+        secondStage.setWidth(400);
+        secondStage.setHeight(300);
+        secondStage.setResizable(false);
+        secondStage.show();
+        secondStage.centerOnScreen();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                Platform.runLater(secondStage::hide);
+            } catch (final Exception e) {
+                logger.log(Level.SEVERE, "Erreur de thread", e);
+            }
+        }).start();
     }
 
     public static void main(String[] args) {
